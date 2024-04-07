fe85f1516506977785c0f42caedfda7cfd1c1e89
Hari Lakshmi A.S <93150869+harini1703@users.noreply.github.com>
2024-04-07 23:13:44 +0530

    Update interface.py

--- interface.py
+++ interface.py
@@ -2,7 +2,7 @@ import pickle
 import streamlit as st
 
 # Load the model
-pickle_in = open('windspeed1.pkl', 'rb')
+pickle_in = open('fbprop.pkl', 'rb')
 clf = pickle.load(pickle_in)
 
 st.header("Streamlined Wind Speed Forecasting: A Web-Based Machine Learning Approach for Wind Mill Operators with Real-Time Data Using Streamlit")
